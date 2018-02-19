dc1e35b22a879f056a612ef4595dfd276bd5d996
Matt Venables <mattvenables@gmail.com>
2018-02-19 13:20:42 -0500

    Add eslint for linting

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "no-empty": [
+      "error",
+      { "allowEmptyCatch": true }
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
